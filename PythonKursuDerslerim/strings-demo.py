9534ad8f43e39fb9d44960e31aa61991b8936af8
Özgür Güngör <ozgurgungoorr@gmail.com>
2024-07-11 22:47:27 +0300

    Add files via upload

---
+++ PythonKursuDerslerim/strings-demo.py
@@ -0,0 +1,10 @@
+website = "https://www.sadikturan.com"
+course = "Python Kursu: Baştan sona python programlama rehberiniz (40 saat)"
+
+result = len(course)
+
+
+print(result)
+
+
+result = website[7:10]
